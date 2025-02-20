81d5dd0a609fa0f13ccc8fcb00344b58a486c7ef
datobhj <bdfutbol@naver.com>
2025-02-20 10:04:32 +0900

    asdf

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -24,20 +24,24 @@ const rateLimits = {
   high: {
     requests: 30, //15 
     window: '60 m'
+  },
+  superHigh: {
+    requests: 15, // 7.5
+    window: '60 m'
   }
 } as const;
 
 // Map models to their rate limit categories (sorted by price per 1m)
 const modelRateLimitCategories: { [key: string]: keyof typeof rateLimits } = {
-  'o1': 'high', // 37.5 per 1m (input : 15 / output : 60)
+  'o1': 'superHigh', // 37.5 per 1m (input : 15 / output : 60)
   'chatgpt-4o-latest': 'high', // 10 per 1m (input : 5 / output : 15)
   'claude-3-5-sonnet-latest': 'high', // 9 per 1m (input : 3 / output : 15)
-  'deepseek-ai/DeepSeek-R1': 'high', // 7 per 1m 
+  // 'deepseek-ai/DeepSeek-R1': 'high', // 7 per 1m 
   // 'gpt-4o': 'high', // 6.5 per 1m (input : 2.5 / output : 10)
   'grok-2-latest': 'high', // 6 per 1m (input : 2 / output : 10)
   'gemini-1.5-pro': 'mid', // free / 4.5 per 1m (input : 2 / output : 7)
   'o3-mini': 'low', // 2.75 per 1m (input : 1.1 / output : 4.4)
-  'deepseek-ai/DeepSeek-V3': 'mid', // 1.25 per 1m 
+  // 'deepseek-ai/DeepSeek-V3': 'mid', // 1.25 per 1m 
   'llama-3.3-70b-versatile': 'low', //0.7 per 1m
   'gemini-2.0-flash': 'low', // free / 0.25 per 1m (input : 0.1 / output : 0.4)
   'deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free': 'low', // free
