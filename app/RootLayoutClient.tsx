170d16e1ab9da3a82377023f465b2233c96f2b84
datobhj <bdfutbol@naver.com>
2025-03-24 23:52:45 +0900

    d

--- app/RootLayoutClient.tsx
+++ app/RootLayoutClient.tsx
@@ -67,11 +67,11 @@ export default function RootLayoutClient({
       //   "info",
       //   "Gemini-2-flash-tip"
       // ); 
-      // showAnnouncement(
-      //   "Due to high demand on anthropic models, there may be some delays in response time. We are working on it. In the meantime, please try other models.",
-      //   "warning",
-      //   "anthropic-models-high-request-2"
-      // ); 
+      showAnnouncement(
+        "Due to high demand on anthropic models, there may be some delays in response time. We are working on it. Try other models if you are experiencing delays.",
+        "warning",
+        "anthropic-models-high-request-3"
+      ); 
       // showAnnouncement(
       //   "Due to high demand on Anthropic models, we're temporarily setting high limits on Anthropic models for free users. Please subscribe to get unlimited access. 80% off for the first month.",
       //   "error",
@@ -82,11 +82,11 @@ export default function RootLayoutClient({
       //   "error",
       //   "file-read-disabled"
       // );
-      showAnnouncement(
-        "Anthropic models are currently down. We are working on it. In the meantime, please try other models.",
-        "error",
-        "Anthropic-disabled"
-      );
+      // showAnnouncement(
+      //   "Anthropic models are currently down. We are working on it. In the meantime, please try other models.",
+      //   "error",
+      //   "Anthropic-disabled"
+      // );
       // showAnnouncement(
       //   "SYSTEM UPDATE: chatflix_0.0.1: rate limit update, backend updates, and more",
       //   "info",
